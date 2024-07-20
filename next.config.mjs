51a016e4700b670c8bc9eb02a1fe79dfa8b3a18a
Yutthana Siphuengchai <yutthana.s@outlook.com>
2024-07-21 05:54:56 +0700

    Configure bundle analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+import bundleAnalyzer from "@next/bundle-analyzer";
+
+// Analyzing your bundles
+// Run the following command to analyze your bundles
+// ANALYZE=true npm run build
+// Ref: https://nextjs.org/docs/app/building-your-application/optimizing/bundle-analyzer
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withBundleAnalyzer(nextConfig);
